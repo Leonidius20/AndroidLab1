10c2fa38b2084753f63646c785481e847b29da48
Leonidius20 <25536411+Leonidius20@users.noreply.github.com>
2021-09-21 18:11:27 +0300

    Initial commit

---
+++ app/src/main/java/ua/leonidius/androidlab1/MainActivity.kt
@@ -0,0 +1,60 @@
+package ua.leonidius.androidlab1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageButton
+import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import androidx.transition.Visibility
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var languagesList: RecyclerView
+    private lateinit var languageEditText: EditText
+    private lateinit var okButton: Button
+    private lateinit var cancelButton: Button
+    private lateinit var expandButton: ImageButton
+
+    private var lastSelectedText: CharSequence = ""
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        languagesList = findViewById(R.id.languages_list)
+        languageEditText = findViewById(R.id.language_edittext)
+        okButton = findViewById(R.id.ok_button)
+        cancelButton = findViewById(R.id.cancel_button)
+        expandButton = findViewById(R.id.collapse_button)
+
+        languagesList.let {
+            it.adapter = RecyclerArrayAdapter(this, R.array.languages) {
+                lastSelectedText = (it as TextView).text
+            }
+            it.layoutManager = LinearLayoutManager(this)
+        }
+
+        okButton.setOnClickListener {
+            languageEditText.setText(lastSelectedText)
+        }
+
+        cancelButton.setOnClickListener {
+            languageEditText.text.clear()
+        }
+
+        expandButton.setOnClickListener {
+            with(languagesList) {
+                visibility =
+                    if (visibility == View.INVISIBLE)
+                        View.VISIBLE
+                    else View.INVISIBLE
+            }
+        }
+
+    }
+
+}
